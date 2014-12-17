a5dcdcc30ee9866c6dae3ca04cf244f89c89903e
Kevon Green <kevon.green.profile@gmail.com>
2014-12-16 22:17:46 -0500

    Code refactoring

--- lib/jamrock_stocks/stock_parser.rb
+++ lib/jamrock_stocks/stock_parser.rb
@@ -13,11 +13,7 @@ module JamrockStocks
       stocks_symbols    = page.css(StockConfig::SYMBOLS_HTML_PATH).map { |s| s.text.strip.upcase }
       date, time        = page.css(StockConfig::TIME_HTML_PATH).map { |dt| dt.text.gsub(REGEXP_NON_BREAKING_SPACE, ' ').split[0..1] }.flatten
       stocks_prices     = page.css(StockConfig::PRICE_HTML_PATH).map { |p| p.text.strip.gsub(REGEXP_DOLLAR_SIGN, StockParser::STRING_NO_SPACE) }
-      change_and_volume = page.css(StockConfig::CHANGE_AND_VOLUME_HTML_PATH).map { |cv| cv.text.gsub!(REGEXP_NON_BREAKING_SPACE, ' ').
-                                                                                                sub(/\s/, StockParser::STRING_NO_SPACE).
-                                                                                                sub('CHG:$', StockParser::STRING_NO_SPACE).
-                                                                                                sub('VOL:', StockParser::STRING_NO_SPACE).
-                                                                                                sub(',', StockParser::STRING_NO_SPACE).split(/\s\s/) }
+      change_and_volume = get_volume(page)
 
       (stocks_symbols.count - 1).times do |index|
         stocks << Stock.new(symbol: stocks_symbols[index].to_s,
@@ -27,8 +23,24 @@ module JamrockStocks
       end
 
       data = stocks
-      data =  symbols.map!(&:upcase).map!(&:strip).map{ |symbol| stocks.find {|stock| stock.symbol == symbol} }.compact if symbols.to_a.any?
+      data =  transform_symbols(symbols).map{ |symbol| stocks.find {|stock| stock.symbol == symbol} }.compact if symbols.to_a.any?
       { stocks: data, date: date.freeze, time: time.freeze }
+    end
+
+
+    private
+
+    def transform_symbols(symbols)
+      symbols.map!(&:upcase).map!(&:strip)
+    end
+
+    def get_volume(page)
+      page.css(StockConfig::CHANGE_AND_VOLUME_HTML_PATH).
+          map { |cv| cv.text.gsub!(REGEXP_NON_BREAKING_SPACE, ' ').
+          sub(/\s/, StockParser::STRING_NO_SPACE).
+          sub('CHG:$', StockParser::STRING_NO_SPACE).
+          sub('VOL:', StockParser::STRING_NO_SPACE).
+          sub(',', StockParser::STRING_NO_SPACE).split(/\s\s/) }
     end
   end
 end
