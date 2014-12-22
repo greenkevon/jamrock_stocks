a85df41fe70c8b9910225b74ea4e88974337b873
Kevon Green <kevon.green.profile@gmail.com>
2014-12-21 23:20:56 -0500

    Remove unnecessary spec definitions

--- spec/jamrock_stocks_spec.rb
+++ spec/jamrock_stocks_spec.rb
@@ -2,31 +2,10 @@ require 'rspec'
 require 'spec_helper'
 
 describe 'Stock' do
-
-  it 'should not retrieve an invalid stock' do
-    result = StockClient.fetch_stocks symbols:['MILSS']
-    expect(result[:stocks]).to be_empty
-    expect(result[:stocks].count).to eq(0)
-  end
-
-  it 'should retrieve a single stock' do
-    result = StockClient.fetch_stocks symbols:['MIL']
-    expect(result[:stocks]).not_to be_empty
-    expect(result[:stocks].count).to eq(1)
-    expect(result[:stocks].first.symbol).to eq('MIL')
-    expect(result[:stocks].first.symbol).to eq('MIL')
-  end
-
-  it 'should retrieve 3 stocks' do
-    result = StockClient.fetch_stocks symbols:['MIL','BRG', 'BPOW', 'PEMP']
-    expect(result[:stocks]).not_to be_empty
-    expect(result[:stocks].count).to eq(3)
-    expect(result[:stocks].last.symbol).to eq('BPOW')
-  end
-
   it 'should retrieve all stocks' do
-    result = StockClient.fetch_stocks
-    expect(result[:stocks]).not_to be_empty
-    expect(%w(BRG CBNY CAR CCC) - result[:stocks].map{ |s| s.symbol } ).to be_empty
+    stocks = StockClient.stocks
+    expect(stocks).not_to be_empty
+    expect(stocks.count).to eq(60)
+    expect(%w(BRG CBNY CAR CCC) - stocks.map{ |s| s.symbol } ).to be_empty
   end
 end
